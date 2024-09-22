dda5c991fc71777efcf979ef8868fe1e92e33a5d
Sam Suh <samsuh@gmail.com>
2024-09-22 23:12:40 +0900

    put in checks to confirm authentication flow is working

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import { NextUIProvider } from "@nextui-org/react"
+import { NextUIProvider } from '@nextui-org/react'
+// SessionProvider gives us access to next-auth/react's useSession hook inside a client component to access session object
+import { SessionProvider } from 'next-auth/react'
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
-export default function Providers ({children}: ProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  )
 }
