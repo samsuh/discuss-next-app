6be712e09184589a174c319cfa3a06a56a29deea
Sam Suh <samsuh@gmail.com>
2024-09-23 02:52:21 +0900

    create routing in nextjs app router

---
+++ src/app/projects/[...slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProjectShowPage() {
+  return <div>ProjectShow</div>
+}
---
+++ src/app/projects/[...slug]/posts/new/page.tsx
@@ -0,0 +1,3 @@
+export default function PostCreatePage() {
+  return <div>Create New Post Page</div>
+}
