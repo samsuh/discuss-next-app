117e11cc26d89cc3f70c37a066e48223e3179e2c
Sam Suh <samsuh@gmail.com>
2024-09-24 23:22:48 +0900

    finished header component refactor to extract headerauth to a client component

---
+++ src/components/header-auth.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import {
+  Avatar,
+  Button,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@nextui-org/react'
+import * as actions from '@/actions'
+import { signOut as nextAuthSignOut, useSession } from 'next-auth/react'
+
+export default async function HeaderAuth() {
+  const session = useSession()
+
+  let authContent: React.ReactNode
+  if (session.status === 'loading') {
+    authContent = null
+  } else if (session.data?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.data.user.image || ''} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form
+              action={async () => {
+                await actions.signOut()
+                await nextAuthSignOut({ redirect: false })
+              }}
+            >
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    )
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form
+            action={async () => {
+              await actions.signOut()
+              await nextAuthSignOut({ redirect: false })
+            }}
+          >
+            <Button type="submit" color="primary" variant="flat">
+              Sign Out
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    )
+  }
+
+  return authContent
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,59 +1,14 @@
 import Link from 'next/link'
-import { auth } from '@/auth'
 import {
-  Avatar,
-  Button,
   Input,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
 } from '@nextui-org/react'
-import * as actions from '@/actions'
+import HeaderAuth from './header-auth'
 
-export default async function Header() {
-  const session = await auth()
-
-  let authContent: React.ReactNode
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ''} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    )
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signOut}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Out
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    )
-  }
-
+export default function Header() {
   return (
     <Navbar>
       <NavbarBrand>
@@ -66,7 +21,9 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">{authContent}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   )
 }
