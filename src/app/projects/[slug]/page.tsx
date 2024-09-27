491c2e98af49eb6c7ed979d139a14853f85668a1
Sam Suh <samsuh@gmail.com>
2024-09-27 21:47:01 +0900

    display slug on project show page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,16 @@
-export default function ProjectShowPage() {
-  return <div>ProjectShow</div>
+interface ProjectShowPageProps {
+  params: {
+    slug: string
+  }
+}
+
+export default function ProjectShowPage({ params }: ProjectShowPageProps) {
+  const { slug } = params
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+    </div>
+  )
 }
